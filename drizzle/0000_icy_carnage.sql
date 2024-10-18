b94c8de0a8e8a646e0cebd2a85383bb8b10f90ec
Aaron K Saunders <aaron@clearlyinnovative.com>
2024-10-18 12:42:45 -0400

    update schema

--- drizzle/0000_icy_carnage.sql
+++ drizzle/0000_icy_carnage.sql
@@ -1,11 +1,14 @@
 CREATE TABLE `projects` (
 	`id` integer PRIMARY KEY NOT NULL,
 	`name` text,
+	`description` text,
+	`status` text DEFAULT 'not_started' NOT NULL,
 	`owner_id` integer NOT NULL,
 	FOREIGN KEY (`owner_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
 CREATE TABLE `users` (
 	`id` integer PRIMARY KEY NOT NULL,
-	`full_name` text
+	`full_name` text,
+	`email` text
 );
--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,13 +1,19 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
+// Define the allowed values for the enum column
+const PROJECT_STATUS = ['not_started', 'completed', 'archived', 'in_progress'] as const;
+
 export const users = sqliteTable('users', {
-  id: integer('id').primaryKey(), // 'id' is the column name
+  id: integer('id').primaryKey(),
   fullName: text('full_name'),
+  email: text('email'),
 });
 
 export const projects = sqliteTable('projects', {
-  id: integer('id').primaryKey(), // 'id' is the column name
+  id: integer('id').primaryKey(),
   name: text('name'),
+  description: text('description'),
+  status: text('status', { enum: PROJECT_STATUS }).default('not_started').notNull(),
   ownerId: integer('owner_id')
     .notNull()
     .references(() => users.id),
@@ -18,3 +24,4 @@ export type User = typeof users.$inferSelect;
 export type NewUser = typeof users.$inferInsert;
 export type Project = typeof projects.$inferSelect;
 export type NewProject = typeof projects.$inferInsert;
+export type ProjectStatus = (typeof PROJECT_STATUS)[number];
